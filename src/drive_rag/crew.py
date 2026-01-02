022c762f0283bb5b1e5756e4cbe89ccd0b4f964b
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2026-01-03 00:06:20 +0700

    refactor: restructure crew implementation to use LatestAiDevelopmentCrew class

--- src/drive_rag/crew.py
+++ src/drive_rag/crew.py
@@ -1,10 +1,44 @@
 # src/drive_integration/crew.py
-from crewai import Crew, Process
-from drive_integration.config.agents import drive_agent
-from drive_integration.config.tasks import list_drive_files_task
 
-crew = Crew(
-    agents=[drive_agent],
-    tasks=[list_drive_files_task],
-    process=Process.sequential,
-)
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+from drive_rag.tools.google_drive_list_tool import list_google_drive_files
+
+
+@CrewBase
+class LatestAiDevelopmentCrew():
+    """LatestAiDevelopment crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def driveRAG(self) -> Agent:
+        return Agent(
+            config=self.agents_config['drive_agent'], # type: ignore[index]
+            verbose=True,
+            tools=[list_google_drive_files]
+        )
+
+    @task
+    def list_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['list_drive_files_task'], # type: ignore[index]
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the LatestAiDevelopment crew"""
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+        )
+
+
+
+
+
